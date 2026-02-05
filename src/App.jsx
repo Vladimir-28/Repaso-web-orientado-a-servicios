ad2280833cbdd593e88cf8b6c45e4cdadd26f06c
Vladimir-28 <20243ds028@utez.edu.mx>
2026-02-05 15:53:31 -0600

    Estructura iterativa

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 
 
 function App() {
 	const [flag, setFlag] = useState(false);
+	const [list, setLIst] = useState(['Ana', 'Luisa', 'Pepe', 'Juan', 'Roberto']);
 
 	const handleValue = () => {
 		setFlag(!flag);
@@ -18,6 +19,21 @@ function App() {
 			) : (
 				<div className="alert alert-danger">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque iste nisi natus quod, numquam soluta impedit quos est maiores earum ullam animi cum nihil aut eveniet omnis commodi? Totam, recusandae!</div>
 			)}
+
+			<h3 className="mt-4">Estructura iterada</h3>
+			<hr />
+			<div className="row g-3">
+				{list.map((item, index) => (
+					<div key={index} className="col-3">
+						<div className="card border-0 shadow">
+							<div className="card-body">
+								<h4>{item}</h4>
+							</div>
+						</div>
+					</div>
+				))}
+
+			</div>
 
 		</main>
 	)
