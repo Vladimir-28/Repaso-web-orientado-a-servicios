5d859eccba42cadce66da7e13aa3edee6abe9130
Vladimir-28 <20243ds028@utez.edu.mx>
2026-02-05 15:39:47 -0600

    Estructura condicional

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import { useState } from 'react'
 
 
 function App() {
+	const [flag, setFlag] = useState(false);
 
-  return (
-    <>
-      
-    </>
-  )
+	const handleValue = () => {
+		setFlag(!flag);
+	}
+
+	return (
+		<main className='p-4'>
+			<h3>Estructura condicional</h3>
+			<hr />
+			<button type='button' className="btn btn-secondary mb-3" onClick={() => handleValue()}> Cambiar valor</button>
+			{flag ? (
+				<div className="alert alert-success">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, dicta dignissimos? Itaque velit fugiat deserunt repudiandae! Eum, esse sint quis mollitia adipisci soluta in aperiam eos, sunt reiciendis voluptatum eaque?</div>
+			) : (
+				<div className="alert alert-danger">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque iste nisi natus quod, numquam soluta impedit quos est maiores earum ullam animi cum nihil aut eveniet omnis commodi? Totam, recusandae!</div>
+			)}
+
+		</main>
+	)
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '/node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import '/node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
